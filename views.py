a1c58261b9417de14068ac88c41c3ea969746bf5
Lime <laimiux@gmail.com>
2012-12-01 15:21:31 -0800

    k Please enter the commit message for your changes. Lines starting

--- views.py
+++ views.py
@@ -5,11 +5,9 @@ from django.template import RequestContext
 def main_page(request):
     context = RequestContext(request)
     if request.user.is_authenticated():
-        current_user = request.user; 
-             
+        current_user = request.user;           
         return render_to_response('home.html', { 'user' : current_user}, context_instance=context)
     else:
-       
-       return render_to_response('home.html', {'registration_form' : ""}, context_instance=context) 
+       return render_to_response('home.html', context_instance=context) 
     
  
