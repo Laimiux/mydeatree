80aa1ea064b427ee5b0e38de47e281df9a15a2fb
Lime <laimiux@gmail.com>
2012-12-01 20:24:57 -0800

    Fixed a bug where you couldn't edit ideas

--- ideas/views.py
+++ ideas/views.py
@@ -79,7 +79,7 @@ def edit_idea(request, id):
             idea.save()
             return HttpResponseRedirect(idea.get_absolute_url())
     else:
-        form = IdeaForm(initial={'title':  idea.title, 'text' : idea.text, 'category' : idea.category}, user=request.user)
+        form = IdeaForm(user=request.user, initial={'title':  idea.title, 'text' : idea.text })
         
     return render_to_response('idea_edit_form.html', { 'form': form }, context_instance=RequestContext(request))
 
