c7d71fa9424c537b43998999237ea89988217c8e
Lime <laimiux@gmail.com>
2013-03-17 15:41:12 -0700

    Added one more test for ideas

--- tests/favorites/models.py
+++ tests/favorites/models.py
@@ -8,8 +8,6 @@ from favorites.signals import remove_favorite_items
 class SimpleModel(models.Model):
     text = models.CharField(max_length=30)
     #favorites = generic.GenericRelation(FavoriteItem, null=True)
-    
-    
     def delete(self):
         remove_favorite_items(self)
         super(SimpleModel, self).delete()
--- tests/ideas/test_models.py
+++ tests/ideas/test_models.py
@@ -11,14 +11,19 @@ from django.contrib.auth.models import User
 
 from ideas.models import Idea
 
+
+def create_user():
+    user = User(username="laimis", password="blahblah")
+    user.save()
+    return user
+
 class IdeaTest(TestCase):
     def test_parent_idea_delete(self):
         """
         Tests that children ideas are deleted when parent idea
         is removed
         """
-        user = User(username="laimis", password="blahblah")
-        user.save()
+        user = create_user()
         
         parent = Idea(title="Parent Idea", text="this is the parent idea")
         parent.save()
@@ -33,4 +38,21 @@ class IdeaTest(TestCase):
         self.assertRaises(Idea.DoesNotExist, Idea.objects.get,pk=parent_id)
         self.assertRaises(Idea.DoesNotExist, Idea.objects.get,pk=children_id)
         
+    def test_get_children_count(self):
+        """
+        Tests that get_children_count function returns correct number of children
+        """ 
+        user = create_user()
+        
+        idea = Idea(title="Parent Idea", text="this is the parent idea")
+        idea.save()
+        
+        
+        self.assertEqual(idea.get_children_count(), 0)
+        
+        children = idea.idea_set.create(title="Children Idea", text="this is a children idea")
+        children.save()
+        
+        
+        self.assertEqual(idea.get_children_count(), 1)
         
