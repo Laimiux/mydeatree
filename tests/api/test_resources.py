3d6bbf3f0cafb4ca7eb300ec12bc9e9272db8dc9
Lime <laimiux@gmail.com>
2013-03-27 17:32:45 -0700

    Wrote a few API tests

--- tests/api/test_resources.py
+++ tests/api/test_resources.py
@@ -1,10 +1,30 @@
-from django.test import TestCase
+import datetime
+from django.contrib.auth.models import User
+from tastypie.test import ResourceTestCase
 
 
-class ApiTest(TestCase):
-    def test_idea_get(self):
+class IdeaResourceTest(ResourceTestCase):
+    
+    def setUp(self):
+        super(IdeaResourceTest, self).setUp()
         
+        # Create a user.
+        self.username = 'kingninja'
+        self.password = 'couldneverguess'
+        self.user = User.objects.create_user(self.username, 'someemail@gmail.com', self.password)
         
-       pass 
+        self.resource_url = '/api/v1/idea/'
         
-    pass
+    def get_credentials(self):
+        return self.create_basic(username=self.username, password=self.password) 
+    
+    def test_get_list_unauthorized(self):
+        self.assertHttpUnauthorized(self.api_client.get(self.resource_url))
+        
+    def test_get_authorized(self):
+        resp = self.api_client.get(self.resource_url, authentication=self.get_credentials())
+        self.assertEqual(resp.status_code, 200)
+        
+         
+
+        
